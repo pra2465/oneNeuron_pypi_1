51117f1f9fd4829ac72e4fda7de9c4933b82a5c5
pra2465 <you@example.com>
2021-10-14 23:35:32 +0530

    readme complete

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ oneNeuron_pypi_1
 
 # Reference -
 [official python docs](https://packaging.python.org/tutorials/packaging-projects/)
+
+[github docs for github action](https://docs.github.com/en/actions/guides/building-and-testing-python#publishing-to-package-registries)
--- setup.py
+++ setup.py
@@ -14,9 +14,9 @@ setuptools.setup(
     description="its an implementation of Perceptron",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/{USER_NAME}/{PROJECT_NAME}",
+    url=f"https://github.com/{USER_NAME}/{PROJECT_NAME}",
     project_urls={
-        "Bug Tracker": "https://github.com/{USER_NAME}/{PROJECT_NAME}/issues",
+        "Bug Tracker": f"https://github.com/{USER_NAME}/{PROJECT_NAME}/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -25,5 +25,9 @@ setuptools.setup(
     ],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.6",
+    python_requires=">=3.7",
+    install_requires=[
+    "numpy",
+    "tqdm"
+    ]
 )
