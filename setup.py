695b029b9a3917809899706d58008ab227548cb8
pra2465 <you@example.com>
2021-10-15 13:42:07 +0530

    setup updated

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ USER_NAME = "pra2465"
 
 setuptools.setup(
     name=f"{PROJECT_NAME}-{USER_NAME}",
-    version="0.0.1",
+    version="0.0.2",
     author="pra2465",
     author_email="prateek.shankar01@gmail.com",
     description="its an implementation of Perceptron",
